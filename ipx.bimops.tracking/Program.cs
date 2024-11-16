e834d079fc1daca1afc930df324324bfc96e5ee2
Preston Smith <smith.preston2@gmail.com>
2024-11-16 17:44:42 -0500

    moved parser to its own file

--- ipx.bimops.tracking/Program.cs
+++ ipx.bimops.tracking/Program.cs
@@ -17,7 +17,7 @@ public class Program
 
         var filePath = "path/to/your/chunked/csv.csv";
         // var redisCachePath = "path/to/your/chunked/csv.csv"; // for each csv found, find in the redis cache if there' unuploaded data and upload it
-        var chunks = AirtableUploader.ProcessCsvInChunks(filePath, 10, _cts.Token);
+        var chunks = CSVParser.ProcessCsvInChunks(filePath, 10, _cts.Token);
         await AirtableUploader.UploadChunksToAirtable(chunks, 10, CredentialService.GetCreds("appsettings.json"), _cts.Token);
     }
 }
